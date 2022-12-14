f1fd3099979aa1a6309c2130fe323f9f653837d2
Hangly1225 <113439346+Hangly1225@users.noreply.github.com>
2022-12-14 11:52:58 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,18 @@
+class Calculator:
+    def __init__(self):
+        pass
+    
+    def add(self, a, b):
+        return x1 + b
+    
+    def multiply(self, a, b):
+        return a * b
+      
+    def subtract(self, a, b):
+        return a - b
+
+    def divide(self, a, b):
+        if x2 != 0:
+            return a / b
+        
+    
